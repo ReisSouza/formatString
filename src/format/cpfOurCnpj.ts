f4b57ff6d9b88de968282ef93cd6ae70c98f9db4
ReisSouza <reis.souza@outlook.com.br>
2023-04-19 17:14:12 -0300

    feat:

--- src/format/cpfOurCnpj.ts
+++ src/format/cpfOurCnpj.ts
@@ -2,7 +2,7 @@ import { cnpjMask, cpfMask } from '.';
 import { Value } from '../types';
 
 const cpfOurCnpj = (value: Value) => {
-  if (value.length <= 11) {
+  if (value.length <= 14) {
     return cpfMask(value);
   }
   return cnpjMask(value);
